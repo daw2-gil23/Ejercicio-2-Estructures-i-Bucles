041fa544a92d1f6b55c8278cc1b4d091909c5abe
daw2-gil23 <93734349+daw2-gil23@users.noreply.github.com>
2022-09-28 20:56:45 +0200

    Update Ejercicio5.0.php

--- Ejercicio5.0.php
+++ Ejercicio5.0.php
@@ -1,18 +1,46 @@
 <?php
-session_unset();
-$color = $_POST['color'];
-$fuente = $_POST['fuente'];
-$tamanyo = $_POST['tamanyo'];
-setcookie("color",$color,time()+600);
-setcookie("fuente",$fuente,time()+600);
-setcookie("tamanyo",$tamanyo,time()+600);
+    session_unset();
+    if (isset($_POST['coockie'])){
+        $coockie = $_POST['coockie'];
+    }
+    else{
+        $coockie = 0;
+    }
+    $color = $_POST['color'];
+    $fuente = $_POST['fuente'];
+    $tamanyo = $_POST['tamanyo'];
+    setcookie("color",$color,time()+600);
+    setcookie("fuente",$fuente,time()+600);
+    setcookie("tamanyo",$tamanyo,time()+600); 
+    $colorp = $_POST['color'];
+    $fuentep = $_POST['fuente'];
+    $tamanyop = $_POST['tamanyo']
 ?>
-<html>
- <head>
-  <title>Cookie</title>
- </head>
- <body>
-  <a href="Ejercicio5.1.php">Ver tu propio css con Cookie</a>
-  <?php include "Ejercicio2.php";?>
- </body>
-</html>
+
+
+<?php if ($coockie==true): ?>
+    <html>
+        <head>
+        <title>Cookie</title>
+        </head>
+        <body>
+        <a href="Ejercicio5.1.php">Ver tu propio css con Cookie</a>
+        <?php include "Ejercicio2.php";?>
+        </body>
+    </html>
+    </body>
+    </html>
+<?php endif; ?>
+<?php if ($coockie==0): ?>
+    <html>
+    <head>
+    <title>Css</title>
+    </head>
+    <body>
+        <div style="background-color: <?= $color?>;font-size: <?= $tamanyo?>;font-size: <?= $tamanyo?>;">
+            <p>Este es tu css propio</p>
+        </div>
+        <?php include "Ejercicio2.php";?>
+    </body>
+    </html>
+ <?php endif; ?>
